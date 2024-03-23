fe8a2fc5f6c27fdfa4b9a5dcfb4c560a36845c20
Emanuel Torres Clemente <emanueljtc@gmail.com>
2024-03-23 15:26:21 -0400

    realice modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const { envs } = require('./config/env');
-const { startServer } = require('./server/server');
+import { envs } from './config/env.js';
+import { startServer } from './server/server.js';
+// const { startServer } = require('./server/server');
+
 const main = () => {
   startServer({
     port: envs.PORT,
     public_path: envs.PUBLIC_PATH,
-  })
+  });
 };
 
 // Funcion agnostica autoconvocada y asincrona
